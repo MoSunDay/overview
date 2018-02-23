06993f48508c4f8de588e27db0401a72183661c3
Mo_s <mo_s@foxmail.com>
2018-02-23 13:30:58 +0800

    add overview_server control script

---
+++ overview_server.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+function start(){
+   if [ -f "$PIDFILE" ];then
+       echo "$0 is running!"
+       exit 1
+   fi
+   nohup /usr/bin/python "$WEBSOURCE" &> /dev/null &
+   touch $PIDFILE
+   [ $? -eq 0 ] && {
+       echo "$0 start !"
+   }
+}
+function stop(){
+    if [ ! -f "$PIDFILE" ];then
+       echo "$0 not running!"
+       exit 1
+    else
+        ps -ef | grep "overview_server.py" | awk '{print $2}' | xargs kill -9
+        /bin/rm -f "$PIDFILE"
+        [ $? -eq 0 ] && {
+           echo "$0 stop !"
+       }
+    fi
+}
+function restart(){
+    stop
+    sleep 1
+    start
+}
+function init(){
+    WEBHOME="/home/xxx/overview"
+    WEBSOURCE="${WEBHOME}/overview_server.py"
+    WEBLOGDIR="${WEBHOME}/log"
+    WEBLOGFILE="${WEBLOGDIR}/web.log"
+    WEBTEMPDIR="${WEBHOME}/tmp"
+    PIDFILE="${WEBTEMPDIR}/pidfile"
+    [ ! -d ${WEBHOME} ] && {
+        mkdir ${WEBHOME}
+    }
+    [ ! -d ${WEBTEMPDIR} ] && {
+        mkdir ${WEBTEMPDIR}
+    }
+    [ ! -d ${WEBLOGDIR} ] && {
+        mkdir ${WEBLOGDIR}
+    }
+}
+function main(){
+    init
+    [ $# -ne 1 ] && {
+        echo "args error"
+        exit 3
+    }
+    case "$1" in
+        start)
+            start
+        ;;
+        stop)
+                stop
+        ;;
+        restart)
+            restart
+        ;;
+        *)
+            echo "{$0 USE:start|stop|restart}"
+        ;;
+    esac
+}
+main "$@"
